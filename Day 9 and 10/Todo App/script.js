2a7cdcffab68658a7c47420cc3fe780441cac5d4
Pratik Raj Verma <erpratikrajverma@gmail.com>
2026-02-03 23:02:51 +0530

    todo app code updated

---
+++ Day 9 and 10/Todo App/script.js
@@ -0,0 +1,47 @@
+const inputBox = document.querySelector('#inputBox')
+const addBtn = document.querySelector('#addBtn')
+const list = document.querySelector('#list')
+
+
+let listData = []
+
+function createList() {
+    list.innerHTML = ''
+
+    listData.forEach((element, i) => {
+        // creating div
+        const eachList = document.createElement('div')
+        // inserting data into div
+        eachList.innerHTML = ` <p>${element.listValue}</p>
+                <button onclick='DeleteHandler(${i})'>Delete</button>
+                <button onclick='EditHandler(${i})'>Edit</button>`
+        list.append(eachList)
+        eachList.setAttribute('class', 'eachList')
+    })
+}
+
+addBtn.addEventListener('click', () => {
+    if (inputBox.value !== '') {
+        listData.push({
+            listValue: inputBox.value
+        })
+        createList()
+        inputBox.value = ''
+        // console.log(listData)   
+    }
+})
+
+
+function DeleteHandler(i) {
+    listData.splice(i, 1)
+    createList()
+}
+
+function EditHandler(i){
+    console.log(i)
+}
+
+// let arr = [1,2,3,4]
+
+// arr.splice(1,1)
+// console.log(arr)
