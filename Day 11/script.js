e3da3cec57867ca603c648a0eeb46fb3dc9e9e13
Pratik Raj Verma <erpratikrajverma@gmail.com>
2026-02-10 13:46:09 +0530

    dashboard

---
+++ Day 11/script.js
@@ -0,0 +1,36 @@
+// console.log('first')
+// // it will take time 5 sec
+
+// for(let i = 1;i<999999;i++){
+//     for(let j= 1;j<9999;j++){
+
+//     }
+// }
+
+// console.log('second')
+
+
+// //it will take 20 sec
+
+// console.log('third')
+
+
+
+// it will 5 sec
+setTimeout(() => {    
+    console.log('first')
+
+}, 5000);
+
+
+// it will 3 sec
+
+setTimeout(() => { 
+    console.log('second')
+}, 3000);
+
+// it will 1 sec
+
+setTimeout(() => {
+    console.log('third')
+}, 1000);
