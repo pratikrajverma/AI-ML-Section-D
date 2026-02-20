b4437e119f32418f63b342a7cb04d63c6b4be123
Pratik Raj Verma <erpratikrajverma@gmail.com>
2026-02-20 08:18:11 +0530

    #

---
+++ Day 15/script.js
@@ -0,0 +1,18 @@
+// let x = {
+//     name:"Ankit"
+// }
+
+// console.log(x)
+
+
+let y = '{"name":"Rahul"}'
+
+console.log(y)
+
+let b = JSON.parse(y)
+
+console.log(b)
+
+// let a = JSON.stringify(x)
+
+// console.log(a)
